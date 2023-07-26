e7bc9084b24baaeb3ea2a80bf51ee7b1ea367959
Angelo Afeltra <angelo.afeltra99@gmail.com>
2023-07-27 00:51:54 +0200

    Ottimizzazione memoria utilizzata durante la fase di generazione del dataset

--- MetricExtractor/RepoMiner/main.py
+++ MetricExtractor/RepoMiner/main.py
@@ -1,3 +1,7 @@
+import os
+import shutil
+from time import sleep
+
 import pandas
 from tqdm import trange
 import sys
@@ -22,6 +26,14 @@ if __name__ == "__main__":
         "Lista_TF": [],
         "Lista_TNF": []
     }
+
+    if not os.path.exists('../spazioCondiviso/Repository'):
+        os.makedirs('../spazioCondiviso/Repository')
+    if not os.path.exists('../spazioCondiviso/MetricsDetector'):
+        os.makedirs('../spazioCondiviso/MetricsDetector')
+    if not os.path.exists('../spazioCondiviso/Dataset'):
+        os.makedirs('../spazioCondiviso/Dataset')
+
 
     df=pandas.read_csv(path_csv,delimiter=';')
 
@@ -64,21 +76,42 @@ if __name__ == "__main__":
                                                     repositories['Lista_TNF'],
                                                     progressbar):
 
+        #Verifico se la repository è presente gia nel dataset generale
+        if  datasetGeneretor.repositoryExistInDataset('{}_{}'.format(repository,sha)):
+            continue
+
+
         progressbar.set_description("Cloning {}_{}".format(repository,sha))
         progressbar.refresh()
         cloner.clone_repository(repository,url,sha)
+
         progressbar.set_description("Metrics Extract {}_{}".format(repository,sha))
         progressbar.refresh()
         PARAMS = {'repositoryName':'{}_{}'.format(repository,sha)}
         try:
-            #r=requests.get("http://localhost:8080/getFlakinessMetrics",params=PARAMS) #Da utilizzare se non si passa per docker
-            r=requests.get("http://flakinessmetricsdetector:8080/getFlakinessMetrics",params=PARAMS,timeout=300)
-            progressbar.set_description("Generate Dataset {}_{}".format(repository,sha))
-            progressbar.refresh()
-            datasetGeneretor.addRepositoryToDataset('{}_{}'.format(repository,sha),listTF,listTNF)
+            r=requests.get("http://localhost:8080/getFlakinessMetrics",params=PARAMS,timeout=300) #Da utilizzare se non si passa per docker
+            #r=requests.get("http://flakinessmetricsdetector:8080/getFlakinessMetrics",params=PARAMS,timeout=300)
+            if r.text=='true':
+                progressbar.set_description("Generate Dataset {}_{}".format(repository,sha))
+                progressbar.refresh()
+                datasetGeneretor.addRepositoryToDataset('{}_{}'.format(repository,sha),listTF,listTNF)
+
+
+                shutil.rmtree('../spazioCondiviso/Repository/{}_{}'.format(repository,sha))
+                os.remove('../spazioCondiviso/MetricsDetector/{}_{}'.format(repository,sha))
+                os.remove('../spazioCondiviso/MetricsDetector/{}_{}_TestReject'.format(repository,sha))
+            else:
+                shutil.rmtree('../spazioCondiviso/Repository/{}_{}'.format(repository,sha))
+
         except Exception as e:
             progressbar.set_description("Errore di comunicazione col metricsDetector")
             progressbar.refresh()
+            if os.path.exists('../spazioCondiviso/Repository/{}_{}'.format(repository,sha)):
+                shutil.rmtree('../spazioCondiviso/Repository/{}_{}'.format(repository,sha))
+            if os.path.exists('../spazioCondiviso/MetricsDetector/{}_{}'.format(repository,sha)):
+                os.remove('../spazioCondiviso/MetricsDetector/{}_{}'.format(repository,sha))
+            if os.path.exists('../spazioCondiviso/MetricsDetector/{}_{}_TestReject'.format(repository,sha)):
+                os.remove('../spazioCondiviso/MetricsDetector/{}_{}_TestReject'.format(repository,sha))
             continue
 
 
