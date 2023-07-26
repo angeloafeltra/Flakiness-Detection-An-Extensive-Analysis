4333f8f84dc44742e981f78bf9e855c868c69e83
Angelo Afeltra <angelo.afeltra99@gmail.com>
2023-07-26 17:08:10 +0200

    fix errore comunicazione container

--- MetricExtractor/RepoMiner/testConnessione.py
+++ MetricExtractor/RepoMiner/testConnessione.py
@@ -3,7 +3,7 @@ import requests
 
 if __name__ == "__main__":
 
-    r=requests.get("http://flakiness_metrics_detector:8080/testConnectio")
+    r=requests.get("http://flakinessmetricsdetector:8080/testConnection")
     print(r.text)
 
 
